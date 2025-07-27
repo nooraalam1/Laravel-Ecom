ce16695cf638702c4b0eab4b66f1059cffd82552
nooraalam1 <noorsomrat2000@gmail.com>
2025-07-27 16:34:22 +0600

    Navbar added

---
+++ resources/views/Nav/nav.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title></title>
+</head>
+<body>
+        <div class="md:flex justify-center items-center gap-5 py-5 bg-purple-200">
+
+        <div class="md:flex justify-center items-center gap-5">
+            <a href="#" class="btn btn-neutral btn-outline">Home</a>
+            <a href="#" class="btn btn-neutral btn-outline">Products</a>
+            <a href="#" class="btn btn-neutral btn-outline">About</a>
+        </div>
+        @include('welcome')
+        </div>
+</body>
+</html>
